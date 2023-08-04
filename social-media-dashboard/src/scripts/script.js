f1857283fd2eba2627879b2ad08386a99b6a7c46
kanishkasubash <kanishkasubash@gmail.com>
2023-08-05 00:43:09 +0530

    new file:   qr-code-component/.gitignore 	new file:   qr-code-component/README-template.md 	new file:   qr-code-component/README.md 	new file:   qr-code-component/design/desktop-design.jpg 	new file:   qr-code-component/design/desktop-preview.jpg 	new file:   qr-code-component/design/mobile-design.jpg 	new file:   qr-code-component/images/favicon-32x32.png 	new file:   qr-code-component/images/image-qr-code.png 	new file:   qr-code-component/index.html 	new file:   qr-code-component/style-guide.md 	modified:   social-media-dashboard/build/js/main.min.js 	modified:   social-media-dashboard/build/js/main.min.js.map 	modified:   social-media-dashboard/src/scripts/script.js

--- social-media-dashboard/src/scripts/script.js
+++ social-media-dashboard/src/scripts/script.js
@@ -1,19 +1,25 @@
-const btn = document.querySelector("#toggle");
-const prefersSchemeDark = window.matchMedia("(prefers-color-scheme: dark)");
-const prefersSchemeLight = window.matchMedia("(prefers-color-scheme: light)");
+const toggle = document.querySelector("#toggle");
+const prefersColorScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const currentMode = localStorage.getItem("mode");
 
-btn.addEventListener("change", function () {
-    if (prefersSchemeDark.matches) {
+if (currentMode == "dark") {
+    document.body.classList.toggle("dark-mode");
+} else if (currentMode == "light") {
+    document.body.classList.toggle("light-mode");
+}
+
+toggle.addEventListener("change", function () {
+    var mode;
+    if (prefersColorScheme.matches) {
         document.body.classList.toggle("light-mode");
+        mode = document.body.classList.contains("light-mode")
+            ? "light"
+            : "dark";
     } else {
         document.body.classList.toggle("dark-mode");
+        mode = document.body.classList.contains("dark-mode")
+            ? "dark"
+            : "light";
     }
+    localStorage.setItem("mode", mode);
 });
-
-prefersSchemeLight.onchange = (e) => {
-    if (e.matches) {
-        btn.checked = true;
-    } else {
-        btn.checked = false;
-    }
-};
